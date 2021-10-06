1c4edcfeb2d4694083411e647dc9101aea6093a9
KATAM VISHNU KOTESWARA REDDY <koteshkv2240@gmail.com>
2021-10-06 12:45:21 -0400

    Add files via upload

---
+++ cabshareapp/src/main/java/com/cabshare/app/CabShareAppApplication.java
@@ -0,0 +1,13 @@
+package com.cabshare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabShareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabShareAppApplication.class, args);
+	}
+
+}
